cf710d78660e9234988d70f2070f4f7a52c843d2
lokesh1704 <rlokesh390@gmail.com>
2025-01-28 20:15:28 +0530

    commit

---
+++ src/FIZZBUZZ.java
@@ -0,0 +1,16 @@
+public class FIZZBUZZ {
+    public static void main(String[] args) {
+        int i, a;
+        for (i = 1; i <= 100; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FIZZBUZZ" + " " +i);
+            } else if (i % 3 == 0) {
+                System.out.println("FIZZ" +" "+i);
+            } else if (i % 5 == 0) {
+                System.out.println("BUZZ" +" "+i);
+            }
+
+        }
+    }
+}
+
